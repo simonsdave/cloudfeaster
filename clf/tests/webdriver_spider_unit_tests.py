8a6edf9074cbbce9b831c8aefbac9cfa3ed31b70
Dave Simons <simonsdave@gmail.com>
2013-10-20 16:40:00 -0400

    adding unit tests for webdriver_spider.Browser.is_element_present

--- clf/tests/webdriver_spider_unit_tests.py
+++ clf/tests/webdriver_spider_unit_tests.py
@@ -1,7 +1,11 @@
 """This module contains unit tests for the ```webdriver_spider``` module."""
 
+import BaseHTTPServer
 import os
+import SimpleHTTPServer
 import sys
+import threading
+import time
 import unittest
 import uuid
 
@@ -45,3 +49,84 @@ class TestSpider(unittest.TestCase):
             self.assertIsNotNone(self.mock_browser)
             self.assertEqual(self.mock_browser.__enter__.call_count, 1)
             self.assertEqual(self.mock_browser.__exit__.call_count, 1)
+
+
+
+
+class HTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    """Only reason this class exists is to stop writing of log messages
+    to stdout as part of serving up documents."""
+
+    def log_message(format, *arg):
+        """yes this really is meant to be a no-op"""
+        pass
+
+    def do_GET(self):
+        key = self.path[1:]
+        html = HTTPServer.html_pages.get(key, None)
+        if html:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-length", len(html))
+            self.end_headers()
+            self.wfile.write(html)
+        else:
+            self.send_response(404)
+
+
+class HTTPServer(threading.Thread):
+
+    html_pages = {}
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+    def run(self):
+        httpdAddress = ('', 0)
+        httpd = BaseHTTPServer.HTTPServer(
+            httpdAddress,
+            HTTPRequestHandler )
+        self.portNumber = httpd.server_port
+        httpd.serve_forever()
+        "never returns"
+
+    def start(self):
+        threading.Thread.start(self)
+        "give the HTTP server time to start & initialize itself"
+        while 'portNumber' not in self.__dict__:
+            time.sleep(1)
+
+class TestBrowser(unittest.TestCase):
+    """A series of unit tests that validate ```webdriver_spider.Browser```."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls._httpServer = HTTPServer()
+        cls._httpServer.start()
+
+    @classmethod
+    def tearDownClass( cls ):
+        cls._httpServer = None
+
+    def test_is_element_present(self):
+        """Validate ```webdriver_spider.Browser.is_element_present```."""
+        html = (
+            '<html>'
+            '<title>Dave Was Here!!!</title>'
+            '<body>'
+            '<h1 id=42>Dave Was Here!!!</h1>'
+            '</body>'
+            '</html>'
+        )
+        page = "testIsElementPresent.html"
+        HTTPServer.html_pages[page] = html
+        url = "http://127.0.0.1:%d/%s" % (
+            type(self)._httpServer.portNumber,
+            page
+        )
+        with webdriver_spider.Browser(url) as browser:
+            enoughTimeForPageToLoad = 5
+            time.sleep(enoughTimeForPageToLoad)
+            self.assertTrue(browser.is_element_present("//h1[@id='42']"))
+            self.assertFalse(browser.is_element_present("//h1[@id='43']"))
