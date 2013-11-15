ac6b328ce136b5f9b736833118431586299298ba
Dave Simons <simonsdave@gmail.com>
2013-11-14 22:54:59 -0500

    flesh out more spider repo API & CLI

--- clf/spider_repo.py
+++ clf/spider_repo.py
@@ -3,12 +3,18 @@ provides a client interface to the spider repo."""
 
 import logging
 
+import boto
+from boto.s3.connection import S3Connection
+
 
 _logger = logging.getLogger("CLF_%s" % __name__)
 
 
 class SpiderRepo(object):
-    """..."""
+    """The spider repo is realized as an S3 bucket
+    with versioning enabled. This class provides an
+    API for the spider repo and executes the S3
+    operations to implement the spider repo API."""
 
     def __init__(self, name):
         """Constructor."""
@@ -20,30 +26,56 @@ class SpiderRepo(object):
         return self.name
 
     def exists(self):
-        """Returns ```True``` if spider repo exists otherwise
-        returns ```False```."""
+        """Return ```True``` if spider repo exists otherwise
+        return ```False```."""
         try:
-            conn = boto.s3.connection.S3Connection()
-            bucket = conn.get_bucket(bucket_name)
-        except:
-            return False
-
-        return True
+            conn = S3Connection()
+            return conn.lookup(self.name) is not None
+        except Exception as ex:
+            _logger.error(
+                "Error searching for spider repo '%s' - %s",
+                self,
+                str(ex))
+        return False
 
     def create(self):
         """If the spider repo doesn't exist create it.
         Returns ```True``` on success otherwise ```False```."""
-        logger.info("Attempting to spider repo '%s'", self)
+        _logger.info("Attempting to create spider repo '%s'", self)
         if self.exists():
+            _logger.info("Can't recreate spider repo '%s'", self)
             return False
         try:
-            conn = boto.s3.connection.S3Connection()
+            conn = S3Connection()
             bucket = conn.create_bucket(
-                bucket_name,
+                self.name,
                 location=boto.s3.connection.Location.DEFAULT,
                 policy='public-read')
-            logger.info("Created spider repo '%s'", self)
+            _logger.info("Created spider repo '%s'", self)
         except Exception as ex:
-            logger.error("Spider repo create error '%s' - %s", self, str(ex))
+            _logger.error("Spider repo create error '%s' - %s", self, str(ex))
+            return False
+        return True
+
+    def delete(self):
+        """If the spider repo exists delete it.
+        Returns ```True``` on success otherwise ```False```."""
+        _logger.info("Attempting to delete spider repo '%s'", self)
+        if not self.exists():
+            _logger.info("Can't delete non-existant spider repo '%s'", self)
+            return False
+        try:
+            conn = S3Connection()
+            bucket = conn.get_bucket(self.name)
+            for key in bucket.get_all_keys():
+                _logger.info(
+                    "Deleting '%s' from spider repo '%s'",
+                    key.name,
+                    self)
+                key.delete()
+            bucket.delete()
+            _logger.info("Deleted spider repo '%s'", self)
+        except Exception as ex:
+            _logger.error("Spider repo delete error '%s' - %s", self, str(ex))
             return False
         return True
