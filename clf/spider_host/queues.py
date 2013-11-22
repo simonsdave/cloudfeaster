37fa092d585dd98c956f19bd4370f886b3c927b8
Dave Simons <simonsdave@gmail.com>
2013-11-21 19:14:27 -0500

    adding some psudeo code to process()

--- clf/spider_host/queues.py
+++ clf/spider_host/queues.py
@@ -17,6 +17,16 @@ class CrawlRequestQueue(clf.queues.CrawlRequestQueue):
 class CrawlRequestMessage(clf.queues.CrawlRequestMessage):
 
     def process(self):
+        # get spider name from self
+        # create spider repo. where does spider repo's name come from?
+        # download spider from spider repo to temp file
+        # import temp file as python module
+        # module should contain a single class that derives from clf.Spider
+        #   so create instance of that spider (no ctr arguments)
+        # call spider's walk() method using args from self
+        # walk() return value is this method's return value.
+        #
+        # this method should not throw an exception
         pass
 
 class CrawlResponseQueue(clf.queues.CrawlResponseQueue):
