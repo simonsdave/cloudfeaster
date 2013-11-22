18f949b9271e7ceaf4c3e904707cfa34cff42081
Dave Simons <simonsdave@gmail.com>
2013-11-21 19:15:44 -0500

    rewrite mainloop.run() unit tests

--- clf/spider_host/tests/mainloop_unit_tests.py
+++ clf/spider_host/tests/mainloop_unit_tests.py
@@ -46,12 +46,16 @@ class TestMainloop(unittest.TestCase):
         """Verify returning a message from the request queue
         is handled correctly by mainloop.run()."""
 
-        mock_message = mock.Mock()
-        def read_message():
-            mainloop.done = True
-            return mock_message
         mock_request_queue = mock.Mock()
-        mock_request_queue.read_message.side_effect = read_message
+
+        mock_messages = [mock.Mock() for i in range(0, 9)]
+        end_mainloop_message = mock.Mock()
+        def end_mainloop():
+            mainloop.done = True
+        end_mainloop_message.process.side_effect = end_mainloop
+        mock_messages.append(end_mainloop_message)
+
+        mock_request_queue.read_message.side_effect = mock_messages
 
         mock_response_queue = mock.Mock()
 
@@ -62,14 +66,21 @@ class TestMainloop(unittest.TestCase):
             mock_response_queue,
             mock_rrsleeper)
 
+        # read_message always returned something should never have slept
+        self.assertEqual(mock_rrsleeper.sleep.call_args_list, [])
+
+        # should have called request queue's read_message()
+        # once for each of the messages in mock_messages
         self.assertEqual(
             mock_request_queue.read_message.call_args_list,
-            [mock.call()])
+            [mock.call() for i in range(0, len(mock_messages))])
 
-        self.assertEqual(
-            mock_message.delete.call_args_list,
-            [mock.call()])
-
-        self.assertEqual(
-            mock_rrsleeper.sleep.call_args_list,
-            [])
+        # each mock message should have its process() and delete()
+        # methods called once with no arguments
+        for mock_message in mock_messages:
+            self.assertEqual(
+                mock_message.process.call_args_list,
+                [mock.call()])
+            self.assertEqual(
+                mock_message.delete.call_args_list,
+                [mock.call()])
