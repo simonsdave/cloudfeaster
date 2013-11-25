8b54bd1e2e9126b6db77994682a40fce2d436b1c
Dave Simons <simonsdave@gmail.com>
2013-11-25 06:10:40 -0500

    start of unit tests for clf.spider_host.queues

---
+++ clf/spider_host/tests/queues_unit_tests.py
@@ -0,0 +1,93 @@
+"""This module contains unit tests for the spider host's queues module."""
+
+import unittest
+
+from clf.spider_host import queues
+
+
+class TestCrawlRequestQueue(unittest.TestCase):
+
+    def test_get_message_class_returns_expected_message_class(self):
+        self.assertEqual(
+            queues.CrawlRequestQueue.get_message_class(),
+            queues.CrawlRequestMessage)
+
+
+class TestCrawlRequestMessage(unittest.TestCase):
+
+    def test_get_schema_returns_expected_schema(self):
+        expected_schema = {
+            "type": "object",
+            "properties": {
+                "uuid": {
+                    "type": "string",
+                    "minLength": 1,
+                },
+                "spider_name": {
+                    "type": "string",
+                    "minLength": 1,
+                },
+                "spider_args": {
+                    "type": "array",
+                    "items": {
+                        "type": "string",
+                        "minLength": 1,
+                    },
+                },
+            },
+            "required": [
+                "uuid",
+                "spider_name",
+                "spider_args",
+            ],
+            "additionalProperties": False,
+        }
+        self.assertEqual(
+            queues.CrawlRequestMessage.get_schema(),
+            expected_schema)
+
+
+class TestCrawlResponseQueue(unittest.TestCase):
+
+    def test_get_message_class_returns_expected_message_class(self):
+        self.assertEqual(
+            queues.CrawlResponseQueue.get_message_class(),
+            queues.CrawlResponseMessage)
+
+
+class TestCrawlResponseMessage(unittest.TestCase):
+
+    def test_get_schema_returns_expected_schema(self):
+        expected_schema = {
+            "type": "object",
+            "properties": {
+                "uuid": {
+                    "type": "string",
+                    "minLength": 1,
+                },
+                "spider_name": {
+                    "type": "string",
+                    "minLength": 1,
+                },
+                "spider_args": {
+                    "type": "array",
+                    "items": {
+                        "type": "string",
+                        "minLength": 1,
+                    },
+                },
+                "crawl_response": {
+                    "type": "object",
+                },
+            },
+            "required": [
+                "uuid",
+                "spider_name",
+                "spider_args",
+                "crawl_response",
+            ],
+            "additionalProperties": False,
+        }
+        self.assertEqual(
+            queues.CrawlResponseMessage.get_schema(),
+            expected_schema)
