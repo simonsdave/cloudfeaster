86e83aee433269f2fb87d07480e4ccf595af28db
Dave Simons <simonsdave@gmail.com>
2013-12-11 21:35:02 -0500

    sb using signal.SIGINT rather than signal.SIGTERM

--- clf/util/tsh.py
+++ clf/util/tsh.py
@@ -1,5 +1,5 @@
 """Super simple module that's only purpose in life is to install a
-SIGTERM handler that writes a message to the logging infrastructure
+SIGINT handler that writes a message to the logging infrastructure
 before the application terminates."""
 
 import signal
@@ -11,11 +11,11 @@ _logger = logging.getLogger("CLF_%s" % __name__)
 
 
 def _term_signal_handler(signalNumber, frame):
-    assert signalNumber == signal.SIGTERM
+    assert signalNumber == signal.SIGINT
     _logger.error("Shutting down ...")
     sys.exit(0)
 
 
 def install():
-    """Install this module's SIGTERM handler."""
-    signal.signal(signal.SIGTERM, _term_signal_handler)
+    """Install this module's SIGINT handler."""
+    signal.signal(signal.SIGINT, _term_signal_handler)
