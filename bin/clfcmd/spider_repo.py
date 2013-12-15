a9eeb406cb3a42cdb50cd6b5f71827db6783d4d6
Dave Simons <simonsdave@gmail.com>
2013-12-15 17:44:16 -0500

    renaming SpiderRepo -> RemoteSpiderRepo

--- bin/clfcmd/spider_repo.py
+++ bin/clfcmd/spider_repo.py
@@ -3,7 +3,7 @@
 import logging
 
 from cmdutil import create_command_to_function_dict
-from clf.spider_repo.spider_repo import SpiderRepo
+from clf.spider_repo.remote_spider_repo import RemoteSpiderRepo
 
 
 _logger = logging.getLogger("CLF_%s" % __name__)
@@ -20,7 +20,7 @@ def doit(usage_func, args):
 
     commands = {}
     commands.update(create_command_to_function_dict(["c", "create"], _create))
-    commands.update(create_command_to_function_dict(["d", "del", "rm", "delete"], _create))
+    commands.update(create_command_to_function_dict(["d", "del", "rm", "delete"], _delete))
     commands.update(create_command_to_function_dict(["ls"], _ls))
     commands.update(create_command_to_function_dict(["upload", "ul", "up", "u"], _upload))
     commands.update(create_command_to_function_dict(["download", "dl", "down", "d"], _upload))
@@ -38,11 +38,11 @@ def _create(usage_func, args):
     
     repo_name = args[0]
 
-    spider_repo = SpiderRepo.get_repo(repo_name)
+    spider_repo = RemoteSpiderRepo.get_repo(repo_name)
     if spider_repo:
         print "Spider repo '%s' already exists" % repo_name
     else:
-        spider_repo = SpiderRepo.create_repo(repo_name)
+        spider_repo = RemoteSpiderRepo.create_repo(repo_name)
         if spider_repo:
             print "Created spider repo '%s'" % spider_repo
         else:
@@ -55,7 +55,7 @@ def _delete(usage_func, args):
 
     repo_name = args[0]
 
-    spider_repo = SpiderRepo.get_repo(repo_name)
+    spider_repo = RemoteSpiderRepo.get_repo(repo_name)
     if spider_repo:
         spider_repo.delete()
         print "Deleted spider repo '%s'" % repo_name
@@ -68,12 +68,12 @@ def _ls(usage_func, args):
         usage_func("%s ls [<repo-name>]" % command_names[0])
 
     if 0 == len(args):
-        for spider_repo in SpiderRepo.get_all_repos():
+        for spider_repo in RemoteSpiderRepo.get_all_repos():
             print spider_repo
     else:
         repo_name = args[0]
 
-        spider_repo = SpiderRepo.get_repo(repo_name)
+        spider_repo = RemoteSpiderRepo.get_repo(repo_name)
         if spider_repo:
             for spider in spider_repo.spiders():
                 print spider
@@ -88,7 +88,7 @@ def _upload(usage_func, args):
     repo_name = args[0]
     spider_file_name = args[1]
 
-    spider_repo = SpiderRepo.get_repo(repo_name)
+    spider_repo = RemoteSpiderRepo.get_repo(repo_name)
     if spider_repo:
         if spider_repo.upload_spider(spider_file_name):
             fmt = "Success uploading '%s' to spider repo '%s'"
@@ -107,7 +107,7 @@ def _download(usage_func, args):
     repo_name = args[0]
     spider_name = args[1]
 
-    spider_repo = SpiderRepo.get_repo(repo_name)
+    spider_repo = RemoteSpiderRepo.get_repo(repo_name)
     if spider_repo:
         source_code = spider_repo.download_spider(spider_name)
         if source_code:
