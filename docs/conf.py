3a10044413248c8768c013d595c9b3b64778dbf0
Dave Simons <simonsdave@gmail.com>
2013-12-21 09:27:15 -0500

    resetting docs to the sphinx default template

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 import sys
 import os
 
-import sphinx_bootstrap_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -100,8 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
