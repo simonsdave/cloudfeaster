7848f32ba27be242fc61409c8d5633d0755a748c
Dave Simons <simonsdave@gmail.com>
2013-12-31 11:32:48 -0500

    removed unnecessary comment

--- clf/spider.py
+++ clf/spider.py
@@ -9,7 +9,6 @@ import sys
 
 _logger = logging.getLogger("CLF_%s" % __name__)
 
-"""Something."""
 SC_OK = 000 + 0
 SC_WALK_THREW_EXCEPTION = 400 + 1
 SC_CRAWL_THREW_EXCEPTION = SC_WALK_THREW_EXCEPTION
