259a9bcb05becf0c3deb39e849a760aee2700ff0
Dave Simons <simonsdave@gmail.com>
2014-01-16 08:46:15 -0500

    adding docker to spider host provisioning script

--- vagrant/spider_host/provision.sh
+++ vagrant/spider_host/provision.sh
@@ -27,3 +27,10 @@ wget http://chromedriver.storage.googleapis.com/2.6/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod a+rx chromedriver
 mv chromedriver /usr/bin/.
+
+# install docker
+# http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-precise-12-04-lts-64-bit
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get install -y lxc-docker
