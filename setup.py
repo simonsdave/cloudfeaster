a7671e2e446f8e77502bbb40cdb55d417d74370e
Dave Simons <simonsdave@gmail.com>
2014-01-18 08:26:00 -0500

    finally a setup.py that actually works:-)

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
 
 setup(
-    name='clf',
-    py_modules=[
-        'clf.spider',
-        'clf.webdriver_spider',
+    name="clf",
+    packages=[
+        "clf",
+        "clf.clicmd",
+        "clf.util",
+        "clf.spider_host",
+        "clf.spider_repo",
+    ],
+    scripts=[
+        "bin/clf",
+        "bin/spider_host",
     ],
     install_requires=[
-        'selenium==2.35.0',
+        "selenium==2.35.0",
+        "boto==2.15.0",
+        "jsonschema==2.3.0",
+        "python-keyczar==0.71c",
     ],
     version=1.0,
     description="CloudFeaster",
