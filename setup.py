07f80a9baeca29a9a5a9cfa2ca4695c3729ff006
Dave Simons <simonsdave@gmail.com>
2015-05-10 07:58:26 -0400

    put version # in clf/__init__.py

--- setup.py
+++ setup.py
@@ -1,4 +1,39 @@
+#
+# to build the distrubution @ dist/clf-*.*.*.tar.gz
+#
+#   >git clone https://github.com/simonsdave/clf.git
+#   >cd clf
+#   >source cfg4dev
+#   >python setup.py sdist --formats=gztar
+#
+
+import re
+
 from setuptools import setup
+
+#
+# this approach used below to determine ```version``` was inspired by
+# https://github.com/kennethreitz/requests/blob/master/setup.py#L31
+#
+# why this complexity? wanted version number to be available in the
+# a runtime.
+#
+# the code below assumes the distribution is being built with the
+# current directory being the directory in which setup.py is stored
+# which should be totally fine 99.9% of the time. not going to add
+# the coode complexity to deal with other scenarios
+#
+reg_ex_pattern = r"__version__\s*=\s*['\"](?P<version>[^'\"]*)['\"]"
+reg_ex = re.compile(reg_ex_pattern)
+version = ""
+with open("clf/__init__.py", "r") as fd:
+    for line in fd:
+        match = reg_ex.match(line)
+        if match:
+            version = match.group("version")
+            break
+if not version:
+    raise Exception("Can't locate clf's version number")
 
 setup(
     name="clf",
@@ -19,7 +54,7 @@ setup(
         "jsonschema==2.3.0",
         "python-keyczar==0.71c",
     ],
-    version=1.0,
+    version=version,
     description="CloudFeaster",
     author="Dave Simons",
     author_email="simonsdave@gmail.com",
