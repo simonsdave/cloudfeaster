b50b91f73710e0306947ded0d7c467e928ceee14
Dave Simons <simonsdave@gmail.com>
2015-05-22 06:43:16 -0400

    make setup.py work again

--- setup.py
+++ setup.py
@@ -39,20 +39,13 @@ setup(
     name="clf",
     packages=[
         "clf",
-        "clf.clicmd",
         "clf.util",
-        "clf.spider_host",
-        "clf.spider_repo",
     ],
     scripts=[
-        "bin/clf",
-        "bin/spider_host",
     ],
     install_requires=[
         "selenium==2.35.0",
-        "boto==2.15.0",
         "jsonschema==2.3.0",
-        "python-keyczar==0.71c",
     ],
     version=version,
     description="CloudFeaster",
