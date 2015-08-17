9ae449aa7bbe519ccf93d83abe11e1f435e90253
Dave Simons <simonsdave@gmail.com>
2015-08-17 09:28:12 -0400

    selenium 2.47.0 -> 2.47.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     ],
     install_requires=[
         "jsonschema==2.5.1",
-        "selenium==2.47.0",
+        "selenium==2.47.1",
     ],
     include_package_data=True,
     version=version,
