7b1ac9d4823bb484d9e93ae02984d3a831f772ad
Dave Simons <simonsdave@gmail.com>
2015-12-13 17:32:37 -0500

    " -> ' in pypi.py

--- samples/pypi.py
+++ samples/pypi.py
@@ -13,7 +13,7 @@ class PyPISpider(spider.Spider):
     @classmethod
     def get_metadata(cls):
         return {
-            "url": "http://pypi-ranking.info/alltime",
+            'url': 'http://pypi-ranking.info/alltime',
         }
 
     def crawl(self):
@@ -21,32 +21,29 @@ class PyPISpider(spider.Spider):
             return self._crawl(browser)
 
     def _crawl(self, browser):
-
         data = {}
         rank = 1
-
-        locator = "//span[@class='list_title']"
-        title_elements = browser.find_elements_by_xpath(locator)
+        title_elements = browser.find_elements_by_xpath("//span[@class='list_title']")
         for title_element in title_elements:
             module = title_element.get_text()
-            locator = ".."
-            link_element = title_element.find_element_by_xpath(locator)
-            link = link_element.get_attribute("href")
-            locator = "../../td[@class='count']/span"
-            count_element = link_element.find_element_by_xpath(locator)
+
+            link_element = title_element.find_element_by_xpath('..')
+            link = link_element.get_attribute('href')
+
+            count_element = link_element.find_element_by_xpath("../../td[@class='count']/span")
             count = count_element.get_int()
 
             data[module] = {
-                "rank": rank,
-                "link": link,
-                "count": count,
+                'rank': rank,
+                'link': link,
+                'count': count,
             }
 
             rank += 1
 
         return spider.CrawlResponseOk(data)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     crawl_args = spider.CLICrawlArgs(PyPISpider)
     crawler = spider.SpiderCrawler(PyPISpider)
     crawl_result = crawler.crawl(*crawl_args)
