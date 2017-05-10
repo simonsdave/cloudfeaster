9933b481f9fb3fee208f4f1d85f95b968af88b37
simonsdave <simosdave@gmail.com>
2017-05-10 11:25:33 -0500

    requests 2.13.0 -> 2.14.1

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ setup(
         #
         "ndg-httpsclient==0.4.2",
         "python-dateutil==2.6.0",
-        "requests==2.13.0",
+        "requests==2.14.1",
         "selenium==3.4.1",
     ],
     include_package_data=True,
