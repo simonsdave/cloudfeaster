1c4d165f08cb1a759092717d278308e276b161d9
simonsdave <simonsdave@gmail.com>
2019-02-08 06:49:56 -0500

    run_sample.sh forces use of $DEV_ENV_DOCKER_IMAGE

--- cloudfeaster/samples/run_sample.sh
+++ cloudfeaster/samples/run_sample.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
 #
-# ./run_sample.sh simonsdave/cloudfeaster:latest xe_exchange_rates.XEExchangeRatesSpider
-# ./run_sample.sh simonsdave/cloudfeaster:latest pypi.PyPISpider <username> <password>
-# ./run_sample.sh simonsdave/cloudfeaster:latest pythonwheels.PythonWheelsSpider
+# ./run_sample.sh xe_exchange_rates.XEExchangeRatesSpider
+# ./run_sample.sh pypi.PyPISpider <username> <password>
+# ./run_sample.sh pythonwheels.PythonWheelsSpider
 #
 
 set -e
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
     echo "usage: $(basename "$0") <docker-image> <spider> [<arg1> <arg2> ... <argN>]" >&2
     exit 1
 fi
 
-DOCKER_IMAGE=${1:-}
-shift
 SPIDER=${1:-}
 shift
 
 docker run \
     --rm \
     --security-opt seccomp:unconfined \
-    "$DOCKER_IMAGE" \
+    --volume "$DEV_ENV_SOURCE_CODE:/app" \
+    "$DEV_ENV_DOCKER_IMAGE" \
     spiderhost.sh "cloudfeaster.samples.$SPIDER" "$@"
 
 exit 0
