b3729d20c4c84b2776103808d6b3dbb350feae50
Dave Simons <simonsdave@gmail.com>
2019-02-12 05:50:59 -0500

    added -i command line option to run_sample.sh

--- cloudfeaster/samples/run_sample.sh
+++ cloudfeaster/samples/run_sample.sh
@@ -5,17 +5,25 @@
 # ./run_sample.sh pypi.PyPISpider <username> <password>
 # ./run_sample.sh pythonwheels.PythonWheelsSpider
 #
+# ./run_sample.sh -i xe_exchange_rates.py
+# ./run_sample.sh -i -r http://host.docker.internal:9515 xe_exchange_rates.py
+#
 
 set -e
 
 NETWORK=bridge
 REMOTE_CHROMEDRIVER=""
+INTERACTIVE=0
 
 while true
 do
     case "${1:-}" in
     # :TODO: how to deal with lower case variable substitution on mac os?
     # case "${1,,}" in
+        -i)
+            shift
+            INTERACTIVE=1
+            ;;
         -r)
             shift
             REMOTE_CHROMEDRIVER=${1:-}
@@ -36,13 +44,25 @@ fi
 SPIDER=${1:-}
 shift
 
-docker run \
-    --rm \
-    --security-opt seccomp:unconfined \
-    --volume "$DEV_ENV_SOURCE_CODE:/app" \
-    -e "CLF_REMOTE_CHROMEDRIVER=$REMOTE_CHROMEDRIVER" \
-    "--network=$NETWORK" \
-    "$DEV_ENV_DOCKER_IMAGE" \
-    spiderhost.sh "cloudfeaster.samples.$SPIDER" "$@"
+if [ "$INTERACTIVE" == "1" ]; then
+    docker run \
+        -it \
+        --rm \
+        --security-opt seccomp:unconfined \
+        --volume "$DEV_ENV_SOURCE_CODE:/app" \
+        -e "CLF_REMOTE_CHROMEDRIVER=$REMOTE_CHROMEDRIVER" \
+        "--network=$NETWORK" \
+        "$DEV_ENV_DOCKER_IMAGE" \
+        "/app/cloudfeaster/samples/$SPIDER" "$@"
+else
+    docker run \
+        --rm \
+        --security-opt seccomp:unconfined \
+        --volume "$DEV_ENV_SOURCE_CODE:/app" \
+        -e "CLF_REMOTE_CHROMEDRIVER=$REMOTE_CHROMEDRIVER" \
+        "--network=$NETWORK" \
+        "$DEV_ENV_DOCKER_IMAGE" \
+        spiderhost.sh "cloudfeaster.samples.$SPIDER" "$@"
+fi
 
 exit 0
