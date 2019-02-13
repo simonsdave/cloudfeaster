886127114205304317e801e364c6faf0565ea215
Dave Simons <simonsdave@gmail.com>
2019-02-13 18:04:59 -0500

    fix travis caused by build-docker-image.sh bug

--- dockerfiles/build-docker-image.sh
+++ dockerfiles/build-docker-image.sh
@@ -15,7 +15,9 @@ fi
 CLOUDFEASTER_TAR_GZ=${1:-}
 IMAGE_NAME=${2:-}
 
-TEMP_DOCKERFILE=$(mktemp)
+CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
+
+TEMP_DOCKERFILE=$CONTEXT_DIR/Dockerfile
 cp "$SCRIPT_DIR_NAME/Dockerfile.template" "$TEMP_DOCKERFILE"
 
 CHROMEDRIVER_VERSION=$(grep __chromedriver_version__ "$SCRIPT_DIR_NAME/../cloudfeaster/__init__.py" | sed -e "s|^.*=[[:space:]]*['\"]||g" | sed -e "s|['\"].*$||g")
@@ -24,8 +26,9 @@ sed \
     -e "s|%CHROMEDRIVER_VERSION%|$CHROMEDRIVER_VERSION|g" \
     "$TEMP_DOCKERFILE"
 
-cp "$CLOUDFEASTER_TAR_GZ" "$SCRIPT_DIR_NAME/cloudfeaster.tar.gz"
-docker build -t "$IMAGE_NAME" --file "$TEMP_DOCKERFILE" "$SCRIPT_DIR_NAME"
-rm "$SCRIPT_DIR_NAME/cloudfeaster.tar.gz"
+cp "$CLOUDFEASTER_TAR_GZ" "$CONTEXT_DIR/cloudfeaster.tar.gz"
+docker build -t "$IMAGE_NAME" --file "$TEMP_DOCKERFILE" "$CONTEXT_DIR"
+
+rm -rf "$CONTEXT_DIR"
 
 exit 0
