a8407c8be29097d5073f00dcdff6c614b0a95707
Dave Simons <simonsdave@gmail.com>
2019-03-29 06:24:54 -0400

    refactor: circleci docker image tag & push

--- dockerfiles/tag-and-push-docker-image.sh
+++ dockerfiles/tag-and-push-docker-image.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
+#
+# Assuming <existing-image-name> is of the format <hub-user>/<repo-name>:<old-tag>,
+# tag <existing-image-name> as <hub-user>/<repo-name>:<tag>, login to dockerhub using
+# <hub-user> plus <password> and then push <hub-user>/<repo-name>:<tag> to dockerhub.
+#
+
 set -e
 
-if [ $# != 4 ]; then
-    echo "usage: $(basename "$0") <image-name> <tag> <username> <password>" >&2
+if [ $# != 3 ]; then
+    echo "usage: $(basename "$0") <existing-image-name> <tag> <password>" >&2
     exit 1
 fi
 
-IMAGE_NAME=${1:-}
+EXISTING_IMAGE_NAME=${1:-}
 TAG=${2:-}
-USERNAME=${3:-}
-PASSWORD=${4:-}
+PASSWORD=${3:-}
 
-NEW_IMAGE_NAME=${IMAGE_NAME/:*/:$TAG}
+NEW_IMAGE_NAME="${EXISTING_IMAGE_NAME%:*}:${TAG}"
 
-docker tag "$IMAGE_NAME" "$NEW_IMAGE_NAME"
+docker tag "${EXISTING_IMAGE_NAME}" "${NEW_IMAGE_NAME}"
 
-echo "$PASSWORD" | docker login --username="$USERNAME" --password-stdin
-docker push "$NEW_IMAGE_NAME"
+echo "${PASSWORD}" | docker login --username="${NEW_IMAGE_NAME%/*}" --password-stdin
+docker push "${NEW_IMAGE_NAME}"
 
 exit 0
