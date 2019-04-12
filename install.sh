74fe9dc8ec5b41a8fc390f253cf15f1b78be48af
Dave Simons <simonsdave@gmail.com>
2019-04-11 20:25:00 -0400

    feat: first cut of install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#
+# should be run out of the spider repo - typically expect cfg4dev
+# master will maintain reasonble backward compatibility
+#
+# curl -s -L https://raw.githubusercontent.com/simonsdave/cloudfeaster/master/install.sh | bash -s --
+#
+
+set -e
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0")" >&2
+    exit 1
+fi
+
+REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+CLF_VERSION=$(grep cloudfeaster== "${REPO_ROOT_DIR}/setup.py" | sed -e "s|^[[:space:]]*['\"]cloudfeaster==||g" | sed -e "s|['\"].*$||g")
+
+DEV_ENV_VERSION=$(curl -s -L "https://raw.githubusercontent.com/simonsdave/cloudfeaster/v${CLF_VERSION}/dev_env/dev-env-version.txt")
+
+pip install "git+https://github.com/simonsdave/dev-env.git@$DEV_ENV_VERSION"
+
+REPO_DOT_SH=$(command -v repo.sh)
+SCRIPT_INSTALL_DIR=$(dirname "$REPO_DOT_SH")
+
+curl \
+    -s \
+    -L \
+    -o "${SCRIPT_INSTALL_DIR}/run-all-spiders.sh" \
+    "https://raw.githubusercontent.com/simonsdave/cloudfeaster/v${CLF_VERSION}/bin/run-all-spiders.sh"
+
+curl \
+    -s \
+    -L \
+    -o "${SCRIPT_INSTALL_DIR}/run-spiders.sh" \
+    "https://raw.githubusercontent.com/simonsdave/cloudfeaster/v${CLF_VERSION}/bin/run-spiders.sh"
+
+exit 0
