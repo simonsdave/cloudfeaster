ed3710d38fb324ac8b371c7e9b52064da1ffef1e
Dave Simons <simonsdave@gmail.com>
2019-04-27 11:00:12 -0400

    chore: run-sample.sh work with chrome driver log

--- cloudfeaster/samples/run-sample.sh
+++ cloudfeaster/samples/run-sample.sh
@@ -16,18 +16,39 @@ fi
 SPIDER=${1:-}
 shift
 
-# CLF_REMOTE_CHROMEDRIVER=http://host.docker.internal:9515
+# export CLF_REMOTE_CHROMEDRIVER=http://host.docker.internal:9515
 # https://docs.docker.com/docker-for-mac/networking/
 NETWORK=bridge
 if [ ! "${CLF_REMOTE_CHROMEDRIVER:-}" == "" ]; then NETWORK=host; fi
 
+DOCKER_CONTAINER_NAME=$(python -c "import uuid; print uuid.uuid4().hex")
+
+SPIDER_OUTPUT=$(mktemp 2> /dev/null || mktemp -t DAS)
+
 docker run \
-    --rm \
+    --name "${DOCKER_CONTAINER_NAME}" \
     --security-opt seccomp:unconfined \
     --volume "$(repo-root-dir.sh):/app" \
-    -e "CLF_REMOTE_CHROMEDRIVER=$CLF_REMOTE_CHROMEDRIVER" \
-    "--network=$NETWORK" \
-    "$DEV_ENV_DOCKER_IMAGE" \
-    "/app/$(repo.sh -u)/samples/$SPIDER.py" "$@"
+    -e "CLF_REMOTE_CHROMEDRIVER=${CLF_REMOTE_CHROMEDRIVER:-}" \
+    "--network=${NETWORK}" \
+    "${DEV_ENV_DOCKER_IMAGE}" \
+    /bin/bash -c "/app/$(repo.sh -u)/samples/${SPIDER}.py" "$@" >& "${SPIDER_OUTPUT}"
+
+SPIDER_OUTPUT_ARTIFACT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
+
+CHROMEDRIVER_LOG_IN_CONTAINER=$(jq -r ._debug.chromeDriverLog "${SPIDER_OUTPUT}" | sed -e "s|null||g")
+if [ ! "${CHROMEDRIVER_LOG_IN_CONTAINER}" == "" ]; then
+    CHROMEDRIVER_LOG_ON_HOST=${SPIDER_OUTPUT_ARTIFACT_DIR}/$(basename "${CHROMEDRIVER_LOG_IN_CONTAINER}")
+
+    docker container cp \
+        "${DOCKER_CONTAINER_NAME}:${CHROMEDRIVER_LOG_IN_CONTAINER}" \
+        "${CHROMEDRIVER_LOG_ON_HOST}"
+
+    sed -i "" -e "s|${CHROMEDRIVER_LOG_IN_CONTAINER}|${CHROMEDRIVER_LOG_ON_HOST}|" "${SPIDER_OUTPUT}"
+fi
+
+docker container rm "${DOCKER_CONTAINER_NAME}" > /dev/null
+
+cat "${SPIDER_OUTPUT}"
 
 exit 0
