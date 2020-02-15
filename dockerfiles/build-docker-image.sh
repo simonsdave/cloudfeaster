c18e9d18f7a6d312a8f1b990e5d1abf3f957dd21
Dave Simons <simonsdave@gmail.com>
2020-02-15 17:29:32 -0500

    refactor: build-docker-image.sh

--- dockerfiles/build-docker-image.sh
+++ dockerfiles/build-docker-image.sh
@@ -15,26 +15,28 @@ fi
 CLOUDFEASTER_TAR_GZ=${1:-}
 IMAGE_NAME=${2:-}
 
+REPO_ROOT_DIR=$(repo-root-dir.sh)
+
 CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
 
-cp "$SCRIPT_DIR_NAME/../bin/install-chrome.sh" "$CONTEXT_DIR/."
-cp "$SCRIPT_DIR_NAME/../bin/install-chromedriver.sh" "$CONTEXT_DIR/."
+cp "${REPO_ROOT_DIR}/bin/install-chrome.sh" "${CONTEXT_DIR}/."
+cp "${REPO_ROOT_DIR}/bin/install-chromedriver.sh" "${CONTEXT_DIR}/."
 
-TEMP_DOCKERFILE=$CONTEXT_DIR/Dockerfile
-cp "$SCRIPT_DIR_NAME/Dockerfile.template" "$TEMP_DOCKERFILE"
+TEMP_DOCKERFILE=${CONTEXT_DIR}/Dockerfile
+cp "${SCRIPT_DIR_NAME}/Dockerfile.template" "${TEMP_DOCKERFILE}"
 
-DEV_ENV_VERSION=$(cat "$SCRIPT_DIR_NAME/../dev_env/dev-env-version.txt")
+DEV_ENV_VERSION=$(cat "${REPO_ROOT_DIR}/dev_env/dev-env-version.txt")
 if [ "${DEV_ENV_VERSION:-}" == "master" ]; then
     DEV_ENV_VERSION=latest
 fi
 sed \
     -i \
-    -e "s|%DEV_ENV_VERSION%|$DEV_ENV_VERSION|g" \
-    "$TEMP_DOCKERFILE"
+    -e "s|%DEV_ENV_VERSION%|${DEV_ENV_VERSION}|g" \
+    "${TEMP_DOCKERFILE}"
 
-cp "$CLOUDFEASTER_TAR_GZ" "$CONTEXT_DIR/cloudfeaster.tar.gz"
-docker build -t "$IMAGE_NAME" --file "$TEMP_DOCKERFILE" "$CONTEXT_DIR"
+cp "${CLOUDFEASTER_TAR_GZ}" "${CONTEXT_DIR}/cloudfeaster.tar.gz"
+docker build -t "${IMAGE_NAME}" --file "${TEMP_DOCKERFILE}" "${CONTEXT_DIR}"
 
-rm -rf "$CONTEXT_DIR"
+rm -rf "${CONTEXT_DIR}"
 
 exit 0
