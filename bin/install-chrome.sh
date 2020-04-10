2b1bb6a9acee5c2b8c81094ce212255ab18a8804
Dave Simons <simonsdave@gmail.com>
2020-04-10 12:36:59 -0400

    fix: eliminate nasty looking install warning

--- bin/install-chrome.sh
+++ bin/install-chrome.sh
@@ -7,7 +7,7 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
-curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DoNotWarn apt-key add -
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 apt-get update -y
 apt-get install -y google-chrome-stable
