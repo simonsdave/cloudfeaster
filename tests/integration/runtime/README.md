c1b86cb07616caba7a424aea10a3f7a0e5e6802e
Dave Simons <simonsdave@gmail.com>
2020-11-29 21:16:46 -0500

    feat: add (minimal) runtime integration tests

---
+++ tests/integration/runtime/README.md
@@ -0,0 +1,10 @@
+# Integration Tests for Runtime
+
+...
+
+```bash
+~> ./docker_image_integration_tests.sh --verbose simonsdave/cloudfeaster-lite:bindle
+Running 'test_spiders_dot_py'
+Successfully completed 1 integration tests.
+~>
+```
---
+++ tests/integration/runtime/docker_image_integration_tests.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+test_spiders_dot_py() {
+    DOCKER_IMAGE=${1:-}
+    shift
+
+    STDOUT=$(mktemp)
+
+    DOCKER_CONTAINER_NAME=$(openssl rand -hex 16)
+
+    docker run \
+        --name "${DOCKER_CONTAINER_NAME}" \
+        "${DOCKER_IMAGE}" \
+        spiders.py \
+        > "${STDOUT}"
+
+    if [ "$(docker container inspect "${DOCKER_CONTAINER_NAME}" | jq '.[0].State.ExitCode')" != "0" ]; then cat "${STDOUT}" && exit 1; fi
+
+    jq . "${STDOUT}" >& /dev/null || { exit 1; }
+
+    docker container rm "${DOCKER_CONTAINER_NAME}" > /dev/null
+
+    rm "${STDOUT}"
+}
+
+test_wrapper() {
+    TEST_FUNCTION_NAME=${1:-}
+    shift
+    NUMBER_TESTS_RUN=$((NUMBER_TESTS_RUN+1))
+    if [ "1" -eq "${VERBOSE:-0}" ]; then
+        echo "Running '${TEST_FUNCTION_NAME}'"
+    else
+        echo -n "."
+    fi
+    "$TEST_FUNCTION_NAME" "$@"
+}
+
+VERBOSE=0
+
+while true
+do
+    case "${1:-}" in
+        --verbose)
+            shift
+            VERBOSE=1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ $# != 1 ]; then
+    echo "usage: $(basename "$0") [--verbose] <docker image>" >&2
+    exit 1
+fi
+
+DOCKER_IMAGE=${1:-}
+
+NUMBER_TESTS_RUN=0
+test_wrapper test_spiders_dot_py "${DOCKER_IMAGE}"
+if [ "1" -ne "${VERBOSE:-0}" ]; then
+    echo ""
+fi
+echo "Successfully completed $NUMBER_TESTS_RUN integration tests."
+
+exit 0
