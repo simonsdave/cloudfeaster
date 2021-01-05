d4dce3cb54c19f8c28fe62f4fb1fcfae886b1f09
Dave Simons <simonsdave@gmail.com>
2021-01-04 20:57:09 -0500

    refactor: start using ARG in spider dev env Dockerfile

--- dockerfiles/dev-env/build-docker-image.sh
+++ dockerfiles/dev-env/build-docker-image.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-#
-# This script builds Cloudfeaster's docker image
-#
 
 set -e
 
@@ -23,18 +20,12 @@ cp "${REPO_ROOT_DIR}/bin/install-chrome.sh" "${CONTEXT_DIR}/."
 cp "${REPO_ROOT_DIR}/bin/install-chromedriver.sh" "${CONTEXT_DIR}/."
 cp "${CLOUDFEASTER_TAR_GZ}" "${CONTEXT_DIR}/cloudfeaster.tar.gz"
 
-TEMP_DOCKERFILE=$(mktemp 2> /dev/null || mktemp -t DAS)
-sed \
-    -e "s|%CIRCLE_CI_EXECUTOR%|$(get-circle-ci-executor.sh)|g" \
-    < "${SCRIPT_DIR_NAME}/Dockerfile.template" \
-    > "${TEMP_DOCKERFILE}"
-
 docker build \
     -t "${IMAGE_NAME}" \
-    --file "${TEMP_DOCKERFILE}" \
+    --file "${SCRIPT_DIR_NAME}/Dockerfile" \
+    --build-arg "CIRCLE_CI_EXECUTOR=$(get-circle-ci-executor.sh)" \
     "${CONTEXT_DIR}"
 
-rm -f "${TEMP_DOCKERFILE}"
 rm -rf "${CONTEXT_DIR}"
 
 exit 0
