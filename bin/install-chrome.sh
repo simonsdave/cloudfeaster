1c32755f7a4c9bd82b10bd1a15098a471f63ee59
Dave Simons <simonsdave@gmail.com>
2021-01-11 23:44:22 -0500

    refactor: install-chrome.sh bourne shell issue #55

--- bin/install-chrome.sh
+++ bin/install-chrome.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
@@ -31,28 +31,28 @@ do
     esac
 done
 
-if [ $# != 0 ]; then
+if [ $# -ne 0 ]; then
     usage
     exit 1
 fi
 
-if [[ "${CHROME}" == "0" && "${CHROMIUM}" == "0" ]]; then
+if [ ${CHROME} -eq 0 ] && [ ${CHROMIUM} -eq 0 ]; then
     echo "must specify at least one of either --chrome or --chromium command line options" >&2
     exit 1
 fi
 
-if [[ "${CHROME}" == "1" ]]; then
+if [ ${CHROME} -eq 1 ]; then
     echo "Installing Google Chrome"
-    curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DoNotWarn apt-key add -
-    sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-    apt-get update -y
-    apt-get install -y google-chrome-stable
+    # curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DoNotWarn apt-key add -
+    # sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    # apt-get update -y
+    # apt-get install -y google-chrome-stable
 fi
 
-if [[ "${CHROMIUM}" == "1" ]]; then
+if [ ${CHROMIUM} -eq 1 ]; then
     echo "Installing Chromium"
-    apt-get update -y
-    apt-get install -y chromium-browser
+    # apt-get update -y
+    # apt-get install -y chromium-browser
 fi
 
 exit 0
